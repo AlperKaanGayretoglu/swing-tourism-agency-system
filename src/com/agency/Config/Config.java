92b0defc22536df07efc299b001e20762fb63bf9
Alper Kaan Gayretoglu <alperkaangayretoglu@gmail.com>
2023-03-12 18:41:04 +0300

    Established connection with the Database.

---
+++ src/com/agency/Config/Config.java
@@ -0,0 +1,8 @@
+package com.agency.Config;
+
+public class Config {
+    public static final String PROJECT_TITLE = "Tourism Agency";
+    public static final String DB_URL = "jdbc:mysql://localhost/tourism_agency";
+    public static final String DB_USERNAME = "root"; // Change this database username according to your own.
+    public static final String DB_PASSWORD = ""; // Change this password according to your own.
+}
